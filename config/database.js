fd4f0f8310177506b23c767070c8931f2c66e0fc
cam <cameronmcorist@gmail.com>
2023-07-10 19:44:45 +1000

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "greyhound-db"),
+      database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
     },
