4dcc8f6115599ae3771340499f679e97af5f7a13
cam <cameronmcorist@gmail.com>
2023-07-12 09:33:01 +1000

    resend, cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -18,4 +18,24 @@ module.exports = [
       keepHeaderOnError: true,
     },
   },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
---
+++ config/plugins.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: "rocky@wdgwf.com",
+        defaultReplyTo: "hugo@wdgwf.com",
+      },
+    },
+  },
+
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
